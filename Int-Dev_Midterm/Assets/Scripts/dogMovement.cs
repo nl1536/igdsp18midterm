ae0eeb1ed4016ff43efa802cbfe532a069b939dd
Still Noah <nl1536@nyu.edu>
2018-02-20 14:03:20 -0500

    doing more changed stuff

--- Int-Dev_Midterm/Assets/Scripts/dogMovement.cs
+++ Int-Dev_Midterm/Assets/Scripts/dogMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class dogMovement : MonoBehaviour {
+	
 
 	// Use this for initialization
 	void Start () {
---
+++ Int-Dev_Midterm/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+	private float tug;
+	private float rotateSpeed;
+
+// Use this for initialization
+	void Start ()
+	{
+
+		tug = 2f;
+		rotateSpeed = 2f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// TUG INPUT
+
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			GetComponent<Rigidbody>().AddRelativeForce(0f,0f,tug, ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			GetComponent<Rigidbody>().AddRelativeForce(0f,0f,-tug, ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			GetComponent<Rigidbody>().AddRelativeForce(-tug,0f,0f, ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			GetComponent<Rigidbody>().AddRelativeForce(tug,0f,0f, ForceMode.Impulse);
+		}
+		
+		// LOOK INPUT
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			GetComponent<Transform>().eulerAngles += new Vector3(0f, -rotateSpeed, 0f);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			GetComponent<Transform>().eulerAngles += new Vector3(0f, rotateSpeed, 0f);
+		}
+		
+		// LEASH
+
+		GetComponent<LineRenderer>().SetPosition(0, new Vector3(GetComponent<Transform>().position.x,
+																GetComponent<Transform>().position.y,
+																GetComponent<Transform>().position.z));
+		GetComponent<LineRenderer>().SetPosition(1, new Vector3(GameObject.Find("Dog").GetComponent<Transform>().position.x,
+																GameObject.Find("Dog").GetComponent<Transform>().position.y,
+																GameObject.Find("Dog").GetComponent<Transform>().position.z));
+	}
+}
